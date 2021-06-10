51bd663321000a1dbc5cf5b149e00330fa3443e1
Virendra Verma <virendra@inventics.tech>
2021-06-10 23:32:09 +0530

    up

--- ExternalAsset/Cover+Shoot/Scripts/PlayerScripts/BasicBehaviour.cs
+++ ExternalAsset/Cover+Shoot/Scripts/PlayerScripts/BasicBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 // This class manages which player behaviour is active or overriding, and call its local functions.
 // Contains basic setup and common functions used by all the player behaviours.
@@ -64,11 +65,23 @@ public class BasicBehaviour : MonoBehaviour
 		colExtents = GetComponent<Collider>().bounds.extents;
 	}
 
+	[Header("My Run ")]
+	public Image RunImage;
+	public Sprite RunSprite;
+	public Sprite WalkSprite;
 	bool isRunning=false;
 	public void OnAndroidRun()
 	{
-		if(isRunning)isRunning=false;
-		else isRunning=true;
+		if(isRunning)
+		{
+			isRunning=false;
+			RunImage.sprite=RunSprite;
+		}
+		else
+		{
+			isRunning=true;
+			RunImage.sprite=WalkSprite;
+		}
 	}
 	
 	void Update()
