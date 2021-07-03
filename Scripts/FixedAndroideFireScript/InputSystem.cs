a60257833b4d82b77805acb45dccb80dd54d6830
AdarshVre <64330136+AdarshVre@users.noreply.github.com>
2021-07-03 14:48:14 +0530

    update

---
+++ Scripts/FixedAndroideFireScript/InputSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ControlMode {Keyboard = 1,Touch = 2};
+
+public class InputSystem : MonoBehaviour
+{
+	public Camera Cam;
+	public FixedJoystick joystick;
+	 
+	void Update(){
+		float angleV = joystick.Vertical;
+		float angleH= joystick.Horizontal;
+		Quaternion aimRotation = Quaternion.Euler(-angleV, angleH, 0);
+		Cam.gameObject.transform.localRotation = aimRotation;
+	}
+}
